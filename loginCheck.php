072235fdd92e0e935110b627d1c9b65801625ea8
izhostMX <interzonegdl2@gmail.com>
2021-10-06 02:57:37 -0500

    asd234652

---
+++ loginCheck.php
@@ -0,0 +1,20 @@
+<?php 
+	session_start();
+	include_once 'model/conn.php';
+	$usuario = $_POST['username'];
+	$contrasena = $_POST['password'];
+
+    $pass = md5($contrasena);
+	$sentencia = $bd->prepare('select * from usuarios where 
+								codigo_id = ? and password = ?;');
+	$sentencia->execute([$usuario, $pass]);
+	$datos = $sentencia->fetch(PDO::FETCH_OBJ);
+	//print_r($datos);
+
+	if ($datos === FALSE) {
+		header('Location: login.php');
+	}elseif($sentencia->rowCount() == 1){
+		$_SESSION['codigo'] = $datos->codigo_id;
+		header('Location: index.php');
+	}
+?>
